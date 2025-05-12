a45fdff03ebaa37735326f8dcf5f5a3c3bae0545
Svyatoslafff <svjatoslavkom12@gmail.com>
2025-05-12 19:08:29 +0300

    add AuthModal.tsx

---
+++ src/components/Modals/AuthModal.tsx
@@ -0,0 +1,52 @@
+import Modal from 'react-modal';
+import { Formik, Form, Field } from 'formik';
+import type { AuthModalProps } from '../../types/modals';
+
+const loginStyles = {};
+
+const initialValues = {
+    email: '',
+    password: '',
+};
+
+export default function AuthModal({
+    type,
+    isOpen,
+    handleSubmit,
+    setIsOpen,
+}: AuthModalProps) {
+    return (
+        <Modal
+            isOpen={isOpen}
+            // onRequestClose={() => setIsLoginModalOpen(false)}
+            contentLabel="Login"
+            style={loginStyles}
+        >
+            <h1>Login</h1>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+                <Form>
+                    {/* {type === 'register' && (
+                        <label>
+                            <p>Login</p>
+                            <Field name="login" />
+                        </label>
+                    )} */}
+
+                    <label>
+                        <p>Email</p>
+                        <Field name="email" />
+                    </label>
+
+                    <label>
+                        <p>Password</p>
+                        <Field name="password" />
+                    </label>
+                    <button type="submit">
+                        {type === 'register' ? 'Register' : 'Login'}
+                    </button>
+                </Form>
+            </Formik>
+            <button onClick={() => setIsOpen(false)}>Close</button>
+        </Modal>
+    );
+}
